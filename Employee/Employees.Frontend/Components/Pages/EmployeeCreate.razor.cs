00d04b613bfd15cd87b7e81cad4243a678e257b3
paplo <paplo@JUAN>
2025-10-05 13:48:46 -0500

    create and edit

---
+++ Employee/Employees.Frontend/Components/Pages/EmployeeCreate.razor.cs
@@ -0,0 +1,35 @@
+using Employees.Frontend.Repositories;
+using Employees.Shared.Entities;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System.Diagnostics.Metrics;
+
+namespace Employees.Frontend.Components.Pages;
+
+public partial class EmployeeCreate
+{
+    private Employee Employee = new();
+
+    [Inject] private IRepository Repository { get; set; } = null!;
+    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+    [Inject] private ISnackbar Snackbar { get; set; } = null!;
+
+    private async Task CreateAsync()
+    {
+        var responseHttp = await Repository.PostAsync("/api/Employee", Employee);
+        if (responseHttp.Error)
+        {
+            var message = await responseHttp.GetErrorMessageAsync();
+            Snackbar.Add(message!, Severity.Error);
+            return;
+        }
+
+        Return();
+        Snackbar.Add("Registro creado", Severity.Success);
+    }
+
+    private void Return()
+    {
+        NavigationManager.NavigateTo("employees");
+    }
+}
